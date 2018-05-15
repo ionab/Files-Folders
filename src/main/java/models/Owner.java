57a835e1f15947c2676bf01f509d555a2395ce34
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-15 23:01:49 +0100

    adds constructors, getters and setters to Owner

--- src/main/java/models/Owner.java
+++ src/main/java/models/Owner.java
@@ -1,4 +1,60 @@
 package models;
 
+import javax.persistence.*;
+import java.util.List;
+@Entity
+@Table(name ="owners")
+
+
 public class Owner {
+
+
+    private List<Folder> folders;
+    private int id;
+    private String name;
+    private String username;
+
+    public Owner(String name, String username) {
+        this.name = name;
+        this.username = username;
+        this.folders = folders;
+    }
+
+    public Owner() {
+    }
+    @OneToMany(mappedBy = "folders")
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
