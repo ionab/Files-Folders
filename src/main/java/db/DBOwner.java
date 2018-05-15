22a2b61dd09424c03ca9729f045ec3d0048b0599
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-15 23:14:01 +0100

    adds DBowner class

---
+++ src/main/java/db/DBOwner.java
@@ -0,0 +1,26 @@
+package db;
+
+import models.Owner;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class DBOwner {
+
+
+    private static Session session;
+    private static Transaction transaction;
+
+    public static void update(Owner owner){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try{ transaction = session.beginTransaction();
+            session.update(owner);
+            transaction.commit();
+        } catch(HibernateException e){
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+}
